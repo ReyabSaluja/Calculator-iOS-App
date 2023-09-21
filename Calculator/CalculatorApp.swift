36cca693c2979ef443d1e79f165e43945e759727
reyabs17 <114021780+reyabs17@users.noreply.github.com>
2023-09-21 00:47:04 -0400

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Reyab Saluja on 2023-09-21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
